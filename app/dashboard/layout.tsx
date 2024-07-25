118514fd7ed96f9a67e9b8935ca7eddc1731c7c9
RunaUXLabs <runa.uxlabs@gmail.com>
2024-07-25 15:37:44 +0900

    챕터10, 부분사전렌더링 셋팅(Partial Prerendering)

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -16,3 +16,15 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     </div>
   );
 }
+export const experimental_ppr = true;
+/**
+ * 부분사전 렌더링 구현을 위한 PPR활성화
+ * 부분 사전 렌더링은 React의 Suspense를 사용한다.
+ * 특정 조건(데이터 로드 등)이 충족될 때까지애플리케이션의 일부 렌더링을 연기함.
+ * <Suspense></Suspense>로 감싸서 폴백 처리해놓고 해당 옵션을 활성화하면
+ * 부분적으로 정적 셸을 사전렌더링 한다.
+ * 정적 셸은 동적 콘텐츠가 비동기적으로 로드되는 곳에 구멍(홀)을 내어 자리를 마련해 놓음.
+ * 동적 콘텐츠의 렌더링은 사용자가 경로를 요청할 때까지 연기됨.
+ * Next.js는 경로의 정적 부분을 미리 렌더링하고 사용자가 요청할 때까지 동적 부분을 연기하여
+ * 프로덕션 단계에서는 성능 향상을 시킨다.
+ */
--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {};
+const nextConfig = {
+  // 부분사전 렌더링 구현을 위한 PPR활성화, 특정 경로에 PPR을 사용할 수 있다.
+  experimental: {
+    ppr: 'incremental',
+  },
+};
 
 export default nextConfig;
